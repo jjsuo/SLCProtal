9c14a71ddb628a9854f4cb5fb2856b7330b8b9ef
jjsuo <645420682@qq.com>
2016-05-11 17:20:31 +0800

    盛诺一家
    前端和后端一起MVC

---
+++ BusinessEntities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class Account
+    {
+        public string UserId { get; set; }
+
+        //用户名
+        public string UserName { get; set; }
+
+
+
+        //性别
+        public string Gendercode { get; set; }
+
+        //证件类型
+        public string Certype { get; set; }
+
+        //证件号码
+        public string Governmentid { get; set; }
+
+
+        //联系方式
+        public string Phone { get; set; }
+
+        //地址
+        public string Address { get; set; }
+    }
+}
