feca5de48c163bcdd475e066b910f6f4101d2c26
jjsuo <645420682@qq.com>
2016-06-04 14:32:22 +0800

    bug

--- BusinessEntities/SickPeople.cs
+++ BusinessEntities/SickPeople.cs
@@ -9,8 +9,34 @@
 
 namespace BusinessEntities
 {
-    public class SickPeople : Account
+    public class SickPeople 
     {
+        public string UserId { get; set; }
+
+        //用户名
+        public string UserName { get; set; }
+
+
+
+        //性别
+        public string Gendercode { get; set; }
+
+        //证件类型
+        public string Certype { get; set; }
+
+        //证件号码
+        public string Governmentid { get; set; }
+
+
+        //联系方式
+        public string Phone { get; set; }
+
+        //联系方式
+        public string Phone1 { get; set; }
+
+        //地址
+        public string Address { get; set; }
+
         public string SickName { get; set; }
 
         public string Remark { get; set; }
