90a7964fe3083bb998631d05cc4cad8e2f5d2d49
xxf456123xxf <549748120@qq.com>
2016-06-04 14:10:26 +0800

    大部分功能完成

--- SLCProtal/js/template.js
+++ SLCProtal/js/template.js
@@ -2,14 +2,37 @@
     $('.usrBd>div').attr('class', $('.usrBd .usrSubMenu a.act').attr('bgclass') + " clearfix");
 })
 var formOptions = {
-    clearForm: true,
+    isSubmit: !1,
+
+    beforeSubmit: function () {
+        if (formOptions.isSubmit) {
+
+            logger.info("表单正在提交中...");
+            return false;
+        }
+
+        formOptions.isSubmit = !0;
+    },
+    clearForm: false,
     success: function (data) {
         if (data.result == "S") {
             logger.success("提交成功");
+            arguments[3].clearForm();
+            if (typeof formOptions.submitsuccess === 'function') {
+                formOptions.submitsuccess(data);
+                formOptions.submitsuccess = undefined;
+            }
         }
         else {
             logger.error(data.result);
+            if (typeof formOptions.submiterror === 'function') {
+                formOptions.submiterror(data);
+                formOptions.submiterror = undefined;
+            }
         }
     },
-    error: logger.ajaxError.bind(this)
+    error: logger.ajaxError.bind(this),
+    complete: function () {
+        formOptions.isSubmit = !1;
+    }
 };
